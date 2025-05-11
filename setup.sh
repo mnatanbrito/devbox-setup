33cc446f93b66856d0397d6e09d9cf36c6204e58
mnatanbrito <mnatan.brito@gmail.com>
2025-05-11 18:44:07 -0400

    Switches from LunarVim to LazyVim

---
+++ setup.sh
@@ -0,0 +1,32 @@
+# Output colors
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+#################
+# General purpose
+################
+
+# Install brew
+echo "Downloading Brew package manager"
+
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+#################
+# Terminals
+################
+
+# Install iTerm2
+# brew install --cask iterm2
+
+# Install Starship prompt
+#brew install starship
